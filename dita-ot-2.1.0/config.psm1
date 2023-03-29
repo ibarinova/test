726ed9f68b7e8b12cd4f2618bff74960748a1987
Iryna Barinova <ibarinova@intelliarts.com>
2023-03-29 17:40:27 +0300

    Add 210 scripts

---
+++ dita-ot-2.1.0/config.psm1
@@ -0,0 +1,121 @@
+set-strictmode -version 2.0
+
+function PrintAndValidatePath
+{
+	param
+	(
+		[String]$key,
+		[String]$value
+	)
+	
+	If ($value -eq "")
+	{
+		Write-Host "$key is empty"
+	}
+	Else
+	{
+		If (-not (Test-Path $value))
+		{
+			Write-Host "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+			Write-Host "*-* Config.psm1::VALUE OF PARAMETER $key IS NOT A FOLDER"
+			Write-Host "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+		}
+		Write-Host "$key=$value"
+	}
+}
+
+function PrintAndValidateFilePath
+{
+	param
+	(
+		[String]$key,
+		[String]$value
+	)
+	
+	If ($value -eq "")
+	{
+		Write-Host "$key is empty"
+	}
+	Else
+	{
+		If (-not (Test-Path $value))
+		{
+			Write-Host "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+			Write-Host "*-* Config.psm1::VALUE OF PARAMETER $key IS NOT A FILE"
+			Write-Host "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+		}
+		Write-Host "$key=$value"
+	}
+}
+
+function PrintParameter
+{
+	param
+	(
+		[String]$key,
+		[String]$value
+	)
+	
+	If ($value -eq "")
+	{
+		Write-Host "$key is empty!"
+	}
+	Else
+	{
+		Write-Host "$key=$value"
+	}
+}
+
+Write-Host "Config.psm1::Initialising InstallTool values"
+$ENV:PS_BASEPATH="E:\InfoShare\Data\PublishingService"
+$ENV:PS_TOOLPATH="E:\InfoShare\App\Utilities\PublishingService\Tools"
+$ENV:PS_ERROROBJECTLOCATION="E:\InfoShare\App\Utilities\PublishingService\Resources"
+$ENV:PS_DITAOT=(Get-Location).Path
+$ENV:PS_UTILITIES="E:\InfoShare\App\Utilities"
+# 2020-04-13 Dana removing this declaration to force DITAOT to take the Java version specified in BackgroundTaskIso.exe.config
+#$ENV:JAVA_HOME="C:\Program Files\Java\jdk1.8.0_144"
+$ENV:JHHOME="C:\javahelp\jh2.0"
+$ENV:AXF_DIR="C:\Program Files\Antenna House\AHFormatterV70"
+$ENV:AXF_OPT="E:\InfoShare\App\Utilities\AntennaHouse\XSLFormatter\XfoSettings.xml"
+# $ENV:XEP_DIR="C:\Program Files\Antenna House\AHFormatterV65"
+# $ENV:PE_DIR="C:\Program Files\Antenna House\AHFormatterV65"
+$ENV:HHCDIR="C:\Program Files (x86)\HTML Help Workshop"
+$ENV:PDF_FORMATTER="ah"
+
+$ENV:ANT_HOME=$ENV:PS_DITAOT
+$ENV:PATH="$ENV:JAVA_HOME\bin;$ENV:ANT_HOME\bin;$ENV:PATH"
+
+$ENV:CLASSPATH="$ENV:PS_DITAOT\lib;$ENV:PS_DITAOT\lib\dost.jar;$ENV:PS_DITAOT\lib\resolver.jar;$ENV:PS_DITAOT\lib\icu4j.jar;$ENV:CLASSPATH"
+$ENV:CLASSPATH="$ENV:PS_DITAOT\lib\commons-codec-1.4.jar;$ENV:PS_DITAOT\lib\xercesImpl.jar;$ENV:PS_DITAOT\lib\xml-apis.jar;$ENV:CLASSPATH"
+$ENV:CLASSPATH="$ENV:PS_DITAOT\lib\saxon\saxon9.jar;$ENV:PS_DITAOT\lib\saxon\saxon9-dom.jar;$ENV:CLASSPATH"
+
+If ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64")
+{
+  $ENV:ANT_OPTS="-Xmx4096m -Xss20m $ENV:ANT_OPTS"
+}
+Else
+{
+  $ENV:ANT_OPTS="-Xmx1500m -Xss20m $ENV:ANT_OPTS"
+}
+$ENV:ANT_OPTS="$ENV:ANT_OPTS -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
+
+PrintAndValidatePath "PS_BASEPATH" $ENV:PS_BASEPATH
+PrintAndValidatePath "PS_UTILITIES" $ENV:PS_UTILITIES
+PrintAndValidatePath "PS_TOOLPATH" $ENV:PS_TOOLPATH
+PrintAndValidatePath "PS_ERROROBJECTLOCATION" $ENV:PS_ERROROBJECTLOCATION
+PrintAndValidatePath "PS_DITAOT" $ENV:PS_DITAOT
+PrintAndValidatePath "JAVA_HOME" $ENV:JAVA_HOME
+PrintAndValidatePath "JHHOME" $ENV:JHHOME
+PrintAndValidatePath "AXF_DIR" $ENV:AXF_DIR
+PrintAndValidateFilePath "AXF_OPT" $ENV:AXF_OPT
+PrintAndValidatePath "XEP_DIR" $ENV:XEP_DIR
+PrintAndValidatePath "PE_DIR" $ENV:PE_DIR
+PrintAndValidatePath "HHCDIR" $ENV:HHCDIR
+PrintParameter "PDF_FORMATTER" $ENV:PDF_FORMATTER
+PrintParameter "PATH" $ENV:PATH
+PrintParameter "CLASSPATH" $ENV:CLASSPATH
+PrintParameter "ANT_OPTS" $ENV:ANT_OPTS
+PrintParameter "TEMP" $ENV:TEMP
+PrintParameter "TMP" $ENV:TMP
+PrintParameter "PROCESSOR_ARCHITECTURE" $ENV:PROCESSOR_ARCHITECTURE
+PrintParameter "DOMAIN\USERNAME" $ENV:USERDOMAIN\$ENV:USERNAME
---
+++ dita-ot-2.1.0/ishrunditaot.ps1
@@ -0,0 +1,112 @@
+# ishrunditaot.cmd is called from the IshRunDITAOT publishpostprocess plugin
+
+#
+# READING SCRIPT ARGUMENTS
+#  
+param
+(
+	[Parameter(Position=0)]
+	$MapFileLocation,
+	[Parameter(Position=1)]
+	$OutputFolder,
+	[Parameter(Position=2)]
+	$TempFolder,
+	[Parameter(Position=3)]
+	$LogFileLocation,
+	[Parameter(Position=4)]
+	$DITAOTTransType, # e.g. ishditadelivery
+	[Parameter(Position=5)]
+	$DraftComments, # yes/no
+	[Parameter(Position=6)]
+	$Compare, # yes/no
+	[Parameter(Position=7)]
+	$CombinedLanguages, # yes/no
+	[Parameter(Position=8)]
+	$PublishPostProcessContextFileLocation
+)
+
+Write-Host "*********************************"
+Write-Host "ISHRUNDITAOT.ps1"
+Write-Host "*********************************"
+
+$DebugPreference   = "SilentlyContinue"   # Continue or SilentlyContinue
+$VerbosePreference = "SilentlyContinue"   # Continue or SilentlyContinue
+$WarningPreference = "Continue"           # Continue or SilentlyContinue or Stop
+$ProgressPreference= "SilentlyContinue"   # Continue or SilentlyContinue
+$ErrorActionPreference = "Stop"           # Treat all errors as terminating errors
+
+#
+# LOAD MODULES
+#
+Write-Host "Loading Modules..."	
+Import-Module ./config.psm1
+# Custom modules
+Import-Module ./config.custom.psm1
+Import-Module ./dita-ot-ishhtmlhelp.psm1
+Import-Module ./dita-ot-dellemc.psm1
+
+$Dvalidate = "yes"
+	If ($Compare -eq "yes")
+	{
+		# If "Compare=yes" set validate to "no" as the input files for DITA-OT contains the comparison result and may not be valid DITA anymore
+		$Dvalidate = "no"
+	}
+
+if ($DITAOTTransType -eq "ishhtmlhelp" -or $DITAOTTransType -eq "html5-webhelp-emc")
+{
+	RunAnt_IshHtmlHelp $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+}
+
+if ($DITAOTTransType -eq "dell-webhelp-responsive" -or $DITAOTTransType -eq "dellemc-webhelp-responsive.draft" -or $DITAOTTransType -eq "dellemc-webhelp-responsive.rsa")
+{
+	RunAnt_DellEMCWEB $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+}
+
+if ($DITAOTTransType -eq "html5-webhelp-emc-draft" -or $DITAOTTransType -eq "html5-webhelp-rsa-draft" -or $DITAOTTransType -eq "webhelp-draft" -or $DITAOTTransType -eq "webhelp-mobile-draft" -or $DITAOTTransType -eq "webhelp-mobile-rsa-draft" -or $DITAOTTransType -eq "webhelp-rsa-draft" -or $DITAOTTransType -eq "webhelp-single-draft" -or $DITAOTTransType -eq "xhtml-emc-draft" -or $DITAOTTransType -eq "xhtml-mozy-draft" -or $DITAOTTransType -eq "webhelp-responsive.rsa.draft" -or $DITAOTTransType -eq "dellemc-webhelp-responsive.draft")
+{
+	$DraftComments = "yes"
+    RunAnt_DellEMCWEB $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+}
+
+if ($DITAOTTransType -eq "atl" -or $DITAOTTransType -eq "html5-responsive" -or $DITAOTTransType -eq "emc-visualizer")
+{
+	RunAnt_DellEMCGeneric $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+}
+
+if ($DITAOTTransType -eq "qa")
+{
+	RunAnt_DellEMCQA $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+
+}
+
+if ($DITAOTTransType -eq "pdf-emc" -or $DITAOTTransType -eq "emcpdf" -or $DITAOTTransType -eq "cpsdpdf")
+{
+	RunAnt_pdfemcRetainTopic $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+
+}
+
+if ($DITAOTTransType -eq "pdf-insert-emc")
+{
+	RunAnt_pdfemc $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+
+}
+
+if ($DITAOTTransType -eq "word-emc" -or $DITAOTTransType -eq "word-solgen")
+{
+	RunAnt_pdfemc $MapFileLocation $OutputFolder $TempFolder $LogFileLocation $DITAOTTransType $DraftComments $Compare $CombinedLanguages $PublishPostProcessContextFileLocation
+
+}
+
+
+else
+{
+	# If "Compare<>yes" set validate to "yes" as the input files for DITA-OT should contain valid DITA xml
+	
+
+	$command = "ant.bat -Dtranstype=""$DITAOTTransType"" -Dargs.input=""$MapFileLocation"" -Doutput.dir=""$OutputFolder"" -Ddita.temp.dir=""$TempFolder"" -Dargs.draft=""$DraftComments"" -Dclean.temp=no -Dvalidate=$Dvalidate -logfile ""$LogFileLocation"""
+	Write-Host "Running command ""$command"""	
+	& ant.bat "-Dtranstype=$DITAOTTransType" "-Dargs.input=""$MapFileLocation""" "-Doutput.dir=""$OutputFolder""" "-Ddita.temp.dir=""$TempFolder""" "-Dargs.draft=$DraftComments" "-Dclean.temp=no" "-Dvalidate=$Dvalidate" -logfile "$LogFileLocation"
+	$exitCode = $LASTEXITCODE
+	Write-Host "ant completed with exit code $exitCode"
+	exit $exitCode
+}
